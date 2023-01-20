27d95d86bafca87d6aab60816ee5245bdcec7fb9
Damiloju <yusufdamiloju@gmail.com>
2023-01-20 11:48:12 +0200

    first draft of project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Outfunnel PostHog Export Plugin
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { PluginMeta } from '@posthog/plugin-scaffold'
+
+export const setupPlugin = (meta: PluginMeta<any>) => {
+
+}
+
+
+export const exportEvents = (events: any[], meta: PluginMeta<any>) => {
+
+}
