c7bbe7dc1ad0b119e42f53c35c7b4a8d6af4f4ec
Damiloju <yusufdamiloju@gmail.com>
2023-01-23 02:22:22 +0200

    wip

--- src/lib.test.ts
+++ src/lib.test.ts
@@ -1,5 +1,29 @@
+import 'isomorphic-fetch';
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+import { createPageview } from '@posthog/plugin-scaffold/test/utils.js';
 import { validateApiKey, getEventsToIgnore, sendEventToOutfunnel } from './lib';
 
+const baseUri = 'https://d4a6-2001-7d0-831a-a700-15be-d1a5-145f-a6a6.eu.ngrok.io';
+
+const handlers = [
+    // Mock the Outfunnel posthog endpoint
+    rest.post(`${baseUri}/posthog`, async (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({
+                "data": {},
+                "status": 200
+            }))
+    }),
+];
+
+// Setup Outfunnel MSW service
+const mswServer = setupServer(...handlers);
+
+beforeAll(() => mswServer.listen());
+afterEach(() => mswServer.resetHandlers());
+afterAll(() => mswServer.close());
 
 describe('validateApiKey', () => {
     it('throws an error if the API key is empty', () => {
@@ -9,33 +33,31 @@ describe('validateApiKey', () => {
     it('does not throw an error if the API key is not empty', () => {
         expect(() => validateApiKey('123')).not.toThrowError('Invalid API key')
     })
-})
+});
 
 
 describe('getEventsToIgnore', () => {
     it('returns a set of events to ignore', () => {
-        expect(getEventsToIgnore('event1, event2')).toEqual(new Set(['event1', 'event2']))
-    })
+        expect(getEventsToIgnore('event1, event2')).toEqual(new Set(['event1', 'event2']));
+    });
 
     it('returns an empty set if no events are provided', () => {
-        expect(getEventsToIgnore('')).toEqual(new Set())
-    })
-})
+        expect(getEventsToIgnore('')).toEqual(new Set());
+    });
+});
 
 
-// describe('sendEventToOutfunnel', () => {
-//     it('sends an event to Outfunnel', async () => {
-//         const event = {
-//             event: 'test',
-//             properties: {
-//                 test: 'test'
-//             }
-//         };
-//
-//         const apiKey = '12345';
-//
-//         const response = await sendEventToOutfunnel(event, apiKey);
-//
-//         expect(sendEventToOutfunnel(event, apiKey)).not.toThrowError('Error sending event to Outfunnel')
-//     })
-// })
+describe('sendEventToOutfunnel',  () => {
+    let pageviewEvent: any;
+    let apiKey: string;
+
+    beforeEach(() => {
+        pageviewEvent = createPageview()
+        apiKey = '12345'
+
+    });
+
+    it('sends an event to Outfunnel', async () => {
+        await expect(sendEventToOutfunnel(pageviewEvent, apiKey)).resolves.not.toThrowError('Error sending event to Outfunnel')
+    });
+});
--- src/lib.ts
+++ src/lib.ts
@@ -3,7 +3,7 @@ import { Logger } from './types'
 import type { RequestInfo, RequestInit, Response } from 'node-fetch'
 
 
-const OUTFUNNEL_URL = 'https://d4a6-2001-7d0-831a-a700-15be-d1a5-145f-a6a6.eu.ngrok.io';
+const OUTFUNNEL_URL = 'https://d4a6-2001-7d0-831a-a700-15be-d1a5-145f-a6a6.eu.ngrok.io/posthog';
 
 export const PluginLogger: Logger = {
     info: console.info,
