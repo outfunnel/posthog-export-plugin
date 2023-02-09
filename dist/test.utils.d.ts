6117ee7271af6f28682e31dd0646f5d00b118a79
Damiloju <yusufdamiloju@gmail.com>
2023-02-09 11:09:54 +0200

    Remove dist directory

--- dist/test.utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const handlers: import("msw").RestHandler<import("msw/lib/SetupApi-f4099ef3").M<import("msw/lib/SetupApi-f4099ef3").c>>[];
-export declare const setupFailedApiHandler: (mswServer: any) => void;
-export declare const setupUnauthorizedApiHandler: (mswServer: any) => void;
