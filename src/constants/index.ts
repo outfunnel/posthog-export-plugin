f4f52e58b238ab44895ad38ac7d7866742ebec8a
Dami <Damiloju@users.noreply.github.com>
2023-02-22 11:05:02 +0100

    Task 3825 move all code to index (#16)
    * Move types
    
    * Move constants
    
    * Move lib files
    
    * Move tests and export functions to test
    
    * Done with refactor - Delete files not used
    
    * Change plugin entry file and update version

--- src/constants/index.ts
+++
@@ -1,1 +0,0 @@
-export const OUTFUNNEL_URL = 'https://sink.outfunnel.com';
--- src/index.test.ts
+++ src/index.test.ts
@@ -2,8 +2,7 @@ import 'isomorphic-fetch';
 import { PluginEvent } from '@posthog/plugin-scaffold'
 import { createPageview } from '@posthog/plugin-scaffold/test/utils.js';
 import { setupServer } from 'msw/node'
-import { setupPlugin, onEvent } from './index';
-import { PluginConfig } from './types';
+import { setupPlugin, onEvent, validateUserId, getEventsToIgnore, sendEventToOutfunnel, PluginConfig } from './index';
 import { handlers, setupFailedApiHandler, setupUnauthorizedApiHandler } from './test.utils'
 
 const defaultConfig: PluginConfig = {
@@ -96,5 +95,55 @@ describe('Outfunnel Plugin', () => {
         });
     });
 
+});
+
+describe('validateUserId', () => {
+    it('throws an error if the User id is empty', () => {
+        expect(() => validateUserId('')).toThrowError('Invalid Outfunnel user ID')
+    })
+
+    it('throws an error if the User id is not valid object Id', () => {
+        expect(() => validateUserId('')).toThrowError('Invalid Outfunnel user ID')
+    })
+
+    it('does not throw an error if the User ID is valid', () => {
+        expect(() => validateUserId('56cb91bdc3464f14678934ca')).not.toThrowError('Invalid Outfunnel user ID')
+    })
+});
+
+
+describe('getEventsToIgnore', () => {
+    it('returns a set of events to ignore', () => {
+        expect(getEventsToIgnore('event1, event2')).toEqual(new Set(['event1', 'event2']));
+    });
+
+    it('returns an empty set if no events are provided', () => {
+        expect(getEventsToIgnore('')).toEqual(new Set());
+    });
+});
+
+
+describe('sendEventToOutfunnel',  () => {
+    let pageviewEvent: any;
+    let userId: string;
+
+    beforeEach(() => {
+        pageviewEvent = createPageview()
+        userId = '56cb91bdc3464f14678934ca'
+    });
+
+    it('sends an event to Outfunnel', async () => {
+        await expect(sendEventToOutfunnel(pageviewEvent, userId)).resolves.not.toThrowError('Error sending event to Outfunnel')
+    });
+
+    it('throws an error if there is a network problem', async () => {
+        setupFailedApiHandler(mswServer);
+        await expect(sendEventToOutfunnel(pageviewEvent, userId)).rejects.toThrowError('Failed to connect')
+    });
+
+    it('throws an error if the User ID is not authorized', async () => {
+        setupUnauthorizedApiHandler(mswServer);
+        await expect(sendEventToOutfunnel(pageviewEvent, userId)).rejects.toThrowError('Error sending event to Outfunnel')
+    });
 });
 
